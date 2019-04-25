406040e81ad14a8a987ad0082ddc9f5b556906ff
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-04-25 15:09:03 -0600

    update v2

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -24,25 +24,45 @@ let lovesCode = true
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
 //Code here
-
+function sum(num1, num2) {
+	return (num1 + num2);
+}
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
 //Code here
+function lovesCodeChecker(x) {
+	if (x === '') {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
 //Code here
+function oddChecker(num) {
+	if (num % 2 == 0) {
+		return 'the number is even';
+	} else {
+		return 'the number is odd';
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
 //Code Here
+function iLove(name, love) {
+	
+}
 
 //////////////////PROBLEM 8////////////////////
 
